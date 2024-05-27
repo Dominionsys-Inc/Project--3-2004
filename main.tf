7a2da2c623a4d27ce55b81534b73c3c4f9d68507
Muhamdaj11 <151459631+Muhamdaj11@users.noreply.github.com>
2024-05-27 21:29:21 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,7 @@
+
 #resource block
 resource "aws_instance" "my_ec2" {
-  ami           = data.aws_ami.amzlinux2.id
+  ami           = data.aws_ami.ubuntu.id
   instance_type = var.my_instance_type
   key_name      = var.my_key
   count         = 3
