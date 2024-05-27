5bc529573b90b4142bed7336f4a14022fe4e48a0
Muhamdaj11 <151459631+Muhamdaj11@users.noreply.github.com>
2024-05-27 21:28:20 +0100

    Update data.tf

--- data.tf
+++ data.tf
@@ -1,24 +1,17 @@
-data "aws_ami" "amzlinux2" {
+data "aws_ami" "ubuntu" {
   most_recent = true
-  owners      = ["amazon"]
-
+  owners      = ["099720109477"]  # Canonical account ID for Ubuntu AMIs
   filter {
     name   = "name"
-    values = ["amzn2-ami-hvm-*-gp2"]
+    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-20240423"]
   }
-
-  filter {
-    name   = "root-device-type"
-    values = ["ebs"]
-  }
-
   filter {
     name   = "virtualization-type"
     values = ["hvm"]
   }
-
   filter {
-    name   = "architecture"
-    values = ["x86_64"]
+    name   = "root-device-type"
+    values = ["ebs"]
   }
 }
+
