dccd7740cab63348f0f7df67fd105e9ac3220073
Muhamdaj11 <151459631+Muhamdaj11@users.noreply.github.com>
2024-05-27 21:31:22 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,55 +1,28 @@
-variable "aws_region" {
-  type    = string
-  default = "us-west-2"
+#resource block
+resource "aws_instance" "my_ec2" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = var.my_instance_type
+  key_name      = var.my_key
+  count         = 3
+  root_block_device {
+    volume_type = var.root_volume_type
+    volume_size = var.root_volume_size
+  }
+
+  ebs_block_device {
+    device_name = var.additional_ebs_device_name
+    volume_size = var.additional_ebs_volume_size
+  }
+  tags = {
+    "Name" = "Ec2-servers"
+  }
 }
 
-variable "my_ami" {
-  type    = string
-  default = "ami-0cf2b4e024cdb6960"
-}
-
-variable "my_instance_type" {
-  type    = string
-  default = "t2.medium"
-}
-
-variable "my_key" {
-  type    = string
-  default = "Automationkey"
-}
-
-variable "region" {
-  description = "AWS region for the VPC"
-  type        = string
-  default     = "us-west-2"
-}
-
-variable "vpc_cidr_block" {
-  description = "CIDR block for VPC"
-  type        = string
-  default     = "10.0.0.0/16"
-}
-
-variable "root_volume_type" {
-  description = "The type of root volume to use"
-  type        = string
-  default     = "gp2"
-}
-
-variable "root_volume_size" {
-  description = "The size of the root volume in GB"
-  type        = number
-  default     = 20
-}
-
-variable "additional_ebs_device_name" {
-  description = "The device name for the additional EBS volume"
-  type        = string
-  default     = "/dev/sdh"
-}
-
-variable "additional_ebs_volume_size" {
-  description = "The size of the additional EBS volume in GB"
-  type        = number
-  default     = 100
+resource "aws_vpc" "my_vpc" {
+  cidr_block           = var.vpc_cidr_block
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+  tags = {
+    Name = "my-vpc"
+  }
 }
